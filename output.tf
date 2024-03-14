bf90528e78d3c9860852447593c2c2e217b165d0
Kaushik Das <kaushik.das@codeclouds.in>
2024-03-14 13:17:00 +0530

    Added terraform backend configuration

--- output.tf
+++ output.tf
@@ -1,29 +1,29 @@
-# # VPC Module Outputs
-# output "vpc_id" {
-#   value = module.vpc_module.vpc_id
+# VPC Module Outputs
+output "vpc_id" {
+  value = module.vpc_module.vpc_id
+}
+output "aws_vpc_name" {
+  value = module.vpc_module.aws_vpc_name
+}
+output "public_subnet_id" {
+  value = module.vpc_module.public_subnet_id
+}
+output "security_group_id" {
+  value = module.vpc_module.security_group_id
+}
+# output "aws_key_pair" {
+#   value = module.vpc_module.aws_key_pair
 # }
-# output "aws_vpc_name" {
-#   value = module.vpc_module.aws_vpc_name
-# }
-# output "public_subnet_id" {
-#   value = module.vpc_module.public_subnet_id
-# }
-# output "security_group_id" {
-#   value = module.vpc_module.security_group_id
-# }
-# # output "aws_key_pair" {
-# #   value = module.vpc_module.aws_key_pair
-# # }
-# output "elastic_IP" {
-#   value = module.vpc_module.elastic_IP
-# }
-# # ----
+output "elastic_IP" {
+  value = module.vpc_module.elastic_IP
+}
+# ----
 
-# # ALB Module Outputs
-# output "load_balancer_endpoint" {
-#   value = module.alb_module.load_balancer_endpoint
-# }
-# #----
+# ALB Module Outputs
+output "load_balancer_endpoint" {
+  value = module.alb_module.load_balancer_endpoint
+}
+#----
 
 # s3 Module Outputs
 output "bucket_name" {
